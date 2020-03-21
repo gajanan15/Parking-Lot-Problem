93e25d9fa6b83251e6a9cb74d55d1983b7509276
gajanan15 <gajanandesai97@gmail.com>
2020-03-21 22:23:34 +0530

    ADDED : Create Test Case And Code For Unparked The Vehicle

--- src/main/java/com/parkingproblem/ParkingLot.java
+++ src/main/java/com/parkingproblem/ParkingLot.java
@@ -1,7 +1,20 @@
 package com.parkingproblem;
 
 public class ParkingLot {
+    private Object vehicle;
+
     public boolean parkTheVehicle(Object vehicle) {
+        if (this.vehicle != null)
+            return false;
+        this.vehicle = vehicle;
         return true;
+    }
+
+    public boolean unParkTheVehicle(Object vehicle) {
+        if (this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/parkingproblem/ParkingLotTest.java
+++ src/test/java/com/parkingproblem/ParkingLotTest.java
@@ -1,13 +1,36 @@
 package com.parkingproblem;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest {
+    private static ParkingLot parkingLot;
+    public static Object vehicle;
+
+    @Before
+    public void setUp() throws Exception {
+        parkingLot = new ParkingLot();
+        vehicle = new Object();
+    }
+
     @Test
     public void givenParkingLot_WhenVehicleIsParked_ShouldReturnTrue() {
-        ParkingLot parkingLot = new ParkingLot();
-        boolean vehicle = parkingLot.parkTheVehicle(new Object());
-        Assert.assertTrue(vehicle);
+        boolean isParked = parkingLot.parkTheVehicle(vehicle);
+        Assert.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenParkingLot_WhenVehicleAlreadyParked_ShouldReturnFalse() {
+        parkingLot.parkTheVehicle(vehicle);
+        boolean isParked = parkingLot.parkTheVehicle(vehicle);
+        Assert.assertFalse(isParked);
+    }
+
+    @Test
+    public void givenParkingLot_WhenVehicleIsUnparked_ShouldReturnTrue() {
+        parkingLot.parkTheVehicle(vehicle);
+        boolean isUnparked = parkingLot.unParkTheVehicle(vehicle);
+        Assert.assertTrue(isUnparked);
     }
 }
